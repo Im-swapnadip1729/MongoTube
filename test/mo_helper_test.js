82d1e7b70ae1f466a4fa28919a71047970b09fdc
swapnadip98 <123538663+swapnadip98@users.noreply.github.com>
2023-12-02 14:24:58 +0530

    Connection to MongoDB

---
+++ test/mo_helper_test.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;//ES6 promises
+
+
+
+before((done) => { 
+    //connecting with mongo db
+    mongoose.connect("mongodb://localhost/mongotube",{useNewUrlParser:true});
+
+    //after connection successfull
+    mongoose.connection
+        .once("open", () => {
+            // console.log("Connected");
+            done();
+        })
+        .on("error", (error) =>{
+            console.log("Your Error", error);
+        });
+});
